3033a5a7cceeea4c4c57cd8d886daa7b54c9fefb
alchemyunited <mark.simchock@alchemyunited.com>
2014-01-24 11:59:27 -0500

    init

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
-wp-ezclasses-autoload
+WP ezClasses Autoload
 =====================
 
 "Wrapper" plugin to autoload the WP ezClasses framework. WP ezClasses (https://github.com/wpezclasses/) is growing collection of classes (and methods) architected and engineered to fulfill the needs of WordPress theme and plugin developers.
+
+
+
+IMPORTANT - Using WP ezClasses Autoload
+=======================================
+
+You are certainly welcome to use individual WP ezClasses on an as needed basis. It is intentionally uber-granular.
+
+However, one of the founding ideals of the WP ezClasses framework is to reduce redundant code, as well as extend the native WordPress framework. That is, to "facade" (so to speak) native WP and make it quick and easier to work with. If it helps, try to imagine WP ezClasses as WordPress 6.5.
+
+It's important to note that WP ezClasses is *not* targeted to developers looking to market free-standing products in (for example) the WordPress.org theme and/or plugin "stores". If you're going to use WP ezClasses for that then please be sure to rename the WP ezClasses files and classes within your product(s). 
+
+On the other hand, if you're doing custom work for unique WordPress based applications then WP ezClasses is for you. By unique we mean for a client or group of clients. You're not necessarily trying to develop traditional free-standing WP plugins and WP themes for the masses. 
+
+For example, we all agree the same class should not be to in multiple plugins in the same install. Clearly, that is a coding and maintenance nightmare. Instead, a single copy of the class would reside in your WP ezClasses Autoload library. It's there, waiting. Just like the traditional WP frameform.
+
+
+
+The Ideal Implementation of the WP ezClasses Framework
+======================================================
+
+The ideal implementation of the WP ezClasses framework uses the WP ezClasses Autoload plugin. 
+
+The the WP ezClasses Autoload plugin should be installed in the Must Use Plugins folder (mu-plugins).
+
+It is also recommended that you strongly consider using the plugin WP ezMU-Plugins (https://github.com/WPezPlugins/wp-ezmu-plugins). WP ezMU-Plugins will help you to manage and fine-tune the load order and activation of your Must Use Plugins.
+
+It is also recommended you have your own version of WP ezClasses Master Singleton (https://github.com/WPezClasses/wp-ezclasses-master-singleton). All of the classes in WP ezClasses extends this class. There is a copy of WP ezClasses Master Singleton within WP ezClasses Autoload. However that only loads if you don't have your own copy pre-included. In order to maintain autotony, have your own copy. It just make more sense that way.
+
+In short, use WP ezMU-Plugins to load WP ezClasses Master Singleton and then WP ezClasses Autoload. As a matter of fact, when you clone / download WP ezMU-Plugins you'll see that these are actually the example. 
+
+
+
+And Finally...
+==============
+
+As you add (within WP ezClasses Autoload) classes to your personalized version of the WP ezClasses framework, those folders could / should be Git submodules. 
+
+http://www.git-scm.com/book/en/Git-Tools-Submodules
+
+If you're not familiar with submodules, imagine submodules as repos within repos. In this case, WP ezClasses Autoload is the outer "wrapper" repo and the individual WP ezClasses will be submodules within it.
+
+Yes, a bit granular. But that's the point. We want to keep things silo'ed and well organized. We don't want to force anyone to do or use anything they don't want to. In other words, we didn't want to force the whole of the WP ezClasses framework on people. That would be silly, as well as poor architecture.
+
+Note: This also allows the individual classes to be addressed asunqiue and freestanding products, focused and finely tuned, as good classes / products should be. 
+
+There are particulars to submodule'ing WP exClasses into WP ezClasses Autoload. Those details / instructions will be with each class. 
+
+
+p.s. The folder: myclasses
+==========================
+
+Once you get comfortble with WP ezClasses we hope you'll be inspired to think about WP dev the WP ezClasses way. That is, you'll become a WP ezDeveloper. 
+
+All you'll have to do is follow the WP ezClasses naming convention and the WP ezClasses Autoload plugin will load your classes as well.
---
+++ wp-ezclasses-autoload.php
@@ -0,0 +1,153 @@
+<?php
+/*
+Plugin Name: WP ezClasses Autoload
+Plugin URI: http://WPezClasses.com?source="plugin"
+Description: "Wrapper" plugin to autoload the WP ezClasses framework. WP ezClasses (https://github.com/wpezclasses/) is growing collection of classes (and methods) architected and engineered to fulfill the needs of WordPress theme and plugin developers. 
+Version: 0.5.0.0
+Author: Mark Simchock for Alchemy United (http://AlchemyUnited.com)
+Author URI: http://ChiefAlchemist.com?source="plugin"
+License: MIT
+*/
+
+
+/** 
+ * TODO
+ *
+ * TODO (@link http://)
+ *
+ * PHP version 5.3
+ *
+ * LICENSE: TODO 
+ *
+ * @package WP ezClasses
+ * @author Mark Simchock <mark.simchock@alchemyunited.com>
+ * @since 0.5.0
+ * @license TODO
+ */
+ 
+/*
+ * == CHANGE LOG == 
+ *
+ * == 10 Jan 2014 ==
+ * --- added: extends WP_ezClasses_Master_Singleton
+ * --- added: class_alias('WP_ezClasses_Methods_Static', 'WP_ezMethods')
+ */
+
+/*
+ * Note: "global" properties inherited from WP_ezClasses_Master_Singleton
+ *
+ * $_bool_ezc_log 					turns the log (wpezToolsClassesLog() on and off (protected, default: true)
+ * $_bool_ezc_validate				turns off _validation methods (protected, default: true)
+ * $_bool_ezc_apply_filters			if you want to use the filters then set this to true (protected, default: false)
+ */
+
+if ( !defined('ABSPATH') ) {
+	header('HTTP/1.0 403 Forbidden');
+    die();
+}
+
+if ( ! class_exists('WP_ezClasses_Master_Singleton')){
+	require_once('_class-wp-ezclasses-master-singleton/class-wp-ezclasses-master-singleton.php');
+}
+
+if ( ! class_exists('WP_ezClasses_Autoload')){
+	class WP_ezClasses_Autoload extends Class_WP_ezClasses_Master_Singleton{
+	
+		private $_version;
+		private $_url;
+		private	$_path;
+		private $_basename;
+		private $_file;
+		
+		protected function __construct() {}
+		
+		public function ezc_init(){
+		
+			$this->_version = '0.5.0';
+			$this->_url = plugin_dir_url( __FILE__ );
+			$this->_path = plugin_dir_path( __FILE__ );
+			$this->_basename = plugin_basename( __FILE__ );
+			$this->_file = __FILE__ ;
+		
+			add_action('activated_plugin', array(&$this, 'load_this_wp_plugin_first'));
+		
+			spl_autoload_register(null, false);
+		
+			spl_autoload_extensions('.php');
+			
+			spl_autoload_register(array($this, 'wp_ezclasses_autoload'));
+			
+			if ( class_exists('Class_WP_ezClasses_Methods_Static') && ! class_exists('WP_ezMethods')){
+				class_alias('Class_WP_ezClasses_Methods_Static', 'WP_ezMethods');
+			}
+		}
+	
+		/*
+		* IMPORTANT: The WP ezClasses library must load first.
+		*
+		* Thanks => (@link http://wordpress.org/support/topic/how-to-change-plugins-load-order)
+		*/
+		public function load_this_wp_plugin_first(){
+		
+			// get the key value for the 'active_plugins' array
+			$this_plugin = plugin_basename(trim( __FILE__));
+			// get the active plugins
+			$active_plugins = get_option('active_plugins');
+			// find current plugin within that array
+			$this_plugin_key = array_search($this_plugin, $active_plugins);
+			 // if it's 0 it's the first plugin already, no need to continue
+			if ($this_plugin_key) {
+				// remove the plugin from the array of actives
+				array_splice($active_plugins, $this_plugin_key, 1);
+				// and now add it back to the top
+				array_unshift($active_plugins, $this_plugin);
+				// update the option 'active_plugins'
+				update_option('active_plugins', $active_plugins);
+			}
+		}
+	
+		/**
+		 * (@link http://www.phpro.org/tutorials/SPL-Autoload.html)
+		 *
+		 * The classes naming convention allows us to parse the folder struture out of the class / file name. And then use that to define the $file for the require_once()
+		 */
+		private function wp_ezclasses_autoload($str_class){
+
+			$str_class = trim(str_replace('_', '-', strtolower($str_class)));
+			
+			// we only want to autoload ezclasses (and myclasses)
+			if ( strpos($str_class, 'class-wp-ezclasses-') === 0 ||  strpos($str_class, 'class-wp-myclasses-') === 0 ){
+					
+				if ( strpos($str_class, 'class-wp-ezclasses-') === 0 ){
+					$int_offset = strlen('class-wp-ezclasses-');
+					$str_main_folder = 'ezclasses';
+				} else {
+					$int_offset = strlen('class-wp-myclasses-');
+					$str_main_folder = 'myclasses';
+				}
+			
+				$int_pos_directory_lead_underscore = strpos($str_class, '-', $int_offset);
+				$int_pos_directory_trail_underscore = strpos($str_class, '-', $int_pos_directory_lead_underscore) + 1;
+		
+				$str_directory_main = strtolower(substr( $str_class, $int_offset, ($int_pos_directory_lead_underscore - $int_offset) ));
+				$str_directory_sub = strtolower(substr( $str_class, $int_pos_directory_trail_underscore, strlen($str_class) ));
+
+				$str_filename = $str_class . '.php';
+				$str_file =  rtrim($this->_path,'/') . DIRECTORY_SEPARATOR . $str_main_folder . DIRECTORY_SEPARATOR . $str_directory_main . DIRECTORY_SEPARATOR . $str_class . DIRECTORY_SEPARATOR . $str_filename;
+				
+				if ( ! file_exists($str_file)) {
+					return false;
+				}
+							
+				require_once ($str_file);
+			}
+			
+		}
+		
+		public function get_version() {
+			return $this->_version;
+		}
+	}
+}
+$init_wp_ezclasses_autoload = WP_ezClasses_Autoload::ezc_get_instance();
+?>
